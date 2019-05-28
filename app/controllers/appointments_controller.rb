bbe6b3c7f184d56b150e4e80437ca743c229b2ce
Joy Lin <vvlnote@hotmail.com>
2019-05-28 21:21:08 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,7 +2,6 @@ require 'date'
 class AppointmentsController < ApplicationController
 
   def show
-
     @appointment = Appointment.find(params[:id])
     date = DateTime.parse(@appointment.appointment_datetime)
     @formatted_date = date.strftime("%B %d, %Y at %H:%M")
