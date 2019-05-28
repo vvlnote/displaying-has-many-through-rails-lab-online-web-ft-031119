0a75d11433333ee91923f0a4d788a7ee747eaa80
Joy Lin <vvlnote@hotmail.com>
2019-05-28 19:06:28 +0000

    Done.

---
+++ db/migrate/20190528190457_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.string :DateTime
+      t.belongs_to :doctor
+      t.belongs_to :patient
+      t.timestamps
+    end
+  end
+end
